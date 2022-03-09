5eca80472f6ab16d2a6ac45c68d57354ffe4ee03
Kolten Fluckiger <wrtunder@gmail.com>
2022-03-09 14:27:57 -0700

    Removed some stuff and things

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-from setuptools import find_namespace_packages, find_packages
-import pathlib
-
-current_dir = pathlib.Path(__file__).parent.resolve()
 
 setup(name='pyencrypto',
       version='1.0',
       description='Python encryption module to help make the process easier for encryption data and decrypting.',
       author='Kolten Fluckiger',
       author_email='wrtunder@gmail.com',
-      url='',
+      url='https://github.com/koltenfluckiger/pyencrypto',
       include_package_data=True,
       packages=['pyencrypto', 'pyencrypto.crypter'],
       install_requires=['cryptography', 'pathlib'],
